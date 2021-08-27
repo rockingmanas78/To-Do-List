c84cc91a5d4fc855aa1d3b5e8abb9428385b65e5
S Manas <rockingmanas78@gmail.com>
2021-08-27 21:34:13 +0530

    procfile added

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.get("/about", function(req, res){
     res.render("about");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server running at port 3000");
 });             
